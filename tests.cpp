f12526de52f01196d1ea5acf10e1c8b1994b568d
mlavi16 <Maya.lavi11@gmail.com>
2020-11-21 11:09:33 -0500

    sumRange (task B)  tests

--- tests.cpp
+++ tests.cpp
@@ -4,7 +4,17 @@
 
 TEST_CASE("returnRange(left, right)") {
     CHECK(returnRange(-5, 5) == "-5 -4 -3 -2 -1 0 1 2 3 4 5 ");
-    CHECK(returnRange(0, 2) == "0 1 2");
+    CHECK(returnRange(0, 2) == "0 1 2 ");
     CHECK(returnRange(0, 0) == "0 ");
     CHECK(returnRange(5, 4) == "");
+}
+
+TEST_CASE("sumRange(left, right") {
+    CHECK(sumRange(-2, 10) == 52);
+    CHECK(sumRange(1, 3) == 6);
+    CHECK(sumRange(-10, -8) == -27);
+    CHECK(sumRange(4, 4) == 4);
+    CHECK(sumRange(0, 0) == 0);
+    CHECK(sumRange(-3, 3) == 0);
+
 }
