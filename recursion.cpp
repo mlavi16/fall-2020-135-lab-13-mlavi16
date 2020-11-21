f8b0de7938d53db0b58ed25c29236f8968ecb916
mlavi16 <Maya.lavi11@gmail.com>
2020-11-21 11:15:13 -0500

    task C initial commit

--- recursion.cpp
+++ recursion.cpp
@@ -10,7 +10,7 @@
 // }
 
 std::string returnRange(int left, int right) {
-    //return all numbers in range left ≤ x ≤ right, separated by spaces
+    // Returns all numbers in range left ≤ x ≤ right, separated by spaces
     std::string range = "";
     if (left > right) {
         return range;
@@ -21,7 +21,7 @@ std::string returnRange(int left, int right) {
 }
 
 int sumRange(int left, int right) {
-    // computes the sum of all numbers in range left ≤ x ≤ right
+    // Computes the sum of all numbers in range left ≤ x ≤ right
     int range = 0;
     if (left > right) {
         return 0;
@@ -29,4 +29,9 @@ int sumRange(int left, int right) {
         range += sumRange(left + 1, right);
         return range + left;
     }
+}
+
+int sumArray(int *arr, int size) {
+    // Returns the sum of the array's elements
+    return 0;
 }
--- recursion.h
+++ recursion.h
@@ -3,3 +3,4 @@
 
 std::string returnRange(int left, int right);
 int sumRange(int left, int right);
+int sumArray(int *arr, int size);
