9675b722d20e0c52ba89f32a54805aca09d4a5b1
mlavi16 <Maya.lavi11@gmail.com>
2020-11-21 10:36:39 -0500

    task A tests

--- recursion.cpp
+++ recursion.cpp
@@ -3,13 +3,12 @@
 #include "recursion.h"
 
 std::string printRange(int left, int right) {
-    //prints all numbers in range left ≤ x ≤ right, separated by spaces
+    //return all numbers in range left ≤ x ≤ right, separated by spaces
     std::string range;
     return returnRange(left, right, range);
 }
 
 std::string returnRange(int left, int right, std::string range) {
-    //returns all numbers in range left ≤ x ≤ right, separated by spaces
     if (left <= right) {
         range += std::to_string(left) + " ";
         range = returnRange(left + 1, right, range);
--- tests.cpp
+++ tests.cpp
@@ -1,3 +1,10 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "recursion.h"
+
+TEST_CASE("printRange(left, right)") {
+    CHECK(printRange(-5, 5) == "-5 -4 -3 -2 -1 0 1 2 3 4 5 ");
+    CHECK(printRange(0, 2) == "0 1 2");
+    CHECK(printRange(0, 0) == "0 ");
+    CHECK(printRange(5, 4) == "");
+}
